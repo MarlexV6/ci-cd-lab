981c6ceedfdecd949ff7626d810187d2d4c12187
MarlexV6 <arturmiysinovich@gmail.com>
2026-05-27 23:52:27 +0300

    second commit for Software testing lab

---
+++ tests/ui.test.js
@@ -0,0 +1,39 @@
+const { Builder, By } = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+
+let driver;
+
+beforeAll(async () => {
+
+    let options = new firefox.Options();
+
+    driver = await new Builder()
+        .forBrowser('firefox')
+        .setFirefoxOptions(options)
+        .build();
+
+    await driver.get('file://' + __dirname + '/../index.html');
+});
+
+afterAll(async () => {
+    await driver.quit();
+});
+
+test('Проверка заголовка', async () => {
+
+    const title = await driver
+        .findElement(By.id('title'))
+        .getText();
+
+    expect(title).toBe('Форма входа');
+});
+
+test('Проверка кнопки', async () => {
+
+    const button = await driver
+        .findElement(By.id('btn'));
+
+    const text = await button.getText();
+
+    expect(text).toBe('Отправить');
+});
